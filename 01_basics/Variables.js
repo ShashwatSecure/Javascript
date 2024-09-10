c31e75ca1d5537aee890adb3c50b3928b33557c2
Shashwat Kumar <167195777+ShashwatSecure@users.noreply.github.com>
2024-09-10 00:36:46 +0000

    Variables explored

---
+++ 01_basics/Variables.js
@@ -0,0 +1,7 @@
+const userId = 67
+let nameClient = "Vikas"
+//not recommended
+var gender = "male"
+profession = "import/export"
+
+console.log([userId,nameClient,gender,profession])
--- Prarambh.js
+++
@@ -1,1 +0,0 @@
-console.log("Antah asti prarambh")
